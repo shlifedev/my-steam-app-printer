6c761b057b1238d9d7ca2527f78c96126300d184
shlifedev <shlifedev@shlifedevs-MacBook-Pro.local>
2025-01-19 22:56:12 +0900

    ㅈ실행코드 변경

--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
 docker build -t steam_appid_gen .
-docker run -it steam_appid_gen
+docker run -i steam_appid_gen
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh  
 docker build -t steam_appid_gen . 
-docker run -it steam_appid_gen
+docker run -i steam_appid_gen
